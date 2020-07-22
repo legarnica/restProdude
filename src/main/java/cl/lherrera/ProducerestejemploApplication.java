210bf5da812b3ce647ef956364275cbf8261724c
Luis Herrera <l.herrera.garnica@gmail.com>
2020-07-22 09:11:53 -0400

    configuraciOn base rest sqlite-jpa

---
+++ src/main/java/cl/lherrera/ProducerestejemploApplication.java
@@ -0,0 +1,13 @@
+package cl.lherrera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerestejemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerestejemploApplication.class, args);
+	}
+
+}
